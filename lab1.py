20cbdb61f9fb0beb5db40925fa17fada45b65044
Brian Lu <b.d.lu2002@gmail.com>
2023-09-13 19:20:46 -0600

    updated lab1.py

--- lab1.py
+++ lab1.py
@@ -2,6 +2,6 @@ import requests
 
 #print(requests.__version__)
 
-resp = requests.get("http://google.com")
+resp = requests.get("https://raw.githubusercontent.com/bdlu1/lab1/main/lab1.py")
 
-print(resp.txt)
+print(resp.text)
